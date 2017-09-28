0fe4e39b6fc36c4fbd400010113e26b5c60f9a48
Shyonokaze <vinceyuhaopan@sina.com>
2017-09-28 23:31:06 +0800

    Create submit.sh

---
+++ submit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#PBS -N pyh
+#PBS -l nodes=2:ppn=24
+#PBS -q snode
+
+#OPENMPI
+export OPEN_MPI=/public/software/mpi/openmpi/1.6.5/intel
+export PATH=${OPEN_MPI}/bin:$PATH
+export INCLUDE=${OPEN_MPI}/include:$INCLUDE
+export LD_LIBRARY_PATH=${OPEN_MPI}/lib:${OPEN_MPI}/share:/public/software/compiler/intel/composer
+_xe_2015.2.164/compiler/lib/intel64:$LD_LIBRARY_PATH
+
+cd $PBS_O_WORKDIR
+NP=`cat $PBS_NODEFILE | wc -l`
+
+num=88
+
+python /public/home/users/ruc001/work/7-PYH/7-BlackP-MD/1-MD/2aBest/main.py $NP $PBS_NODEFILE $nu
+m
