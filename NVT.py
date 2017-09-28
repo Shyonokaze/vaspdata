d8d49d178a0e76728d0968c5f1c8e9054f70a791
Shyonokaze <vinceyuhaopan@sina.com>
2017-09-28 15:36:14 +0800

    Update NVT.py

--- NVT.py
+++ NVT.py
@@ -4,15 +4,19 @@ Created on Sat Sep 23 10:55:45 2017
 
 @author: pyh
 """
-import sys
-sys.path.append('/public/home/users/ruc001/bin')
 
+import shutil
+import sys
+sys.path.append('/public/home/users/ruc001/bin/aBest')
 import vasp
 
+num=int(sys.argv[1])
 potim,nsw,natom,lc,posa=vasp.readvasprun()
-
 vela=vasp.velcal(lc,posa,potim)
-fvmax,fvmin=vasp.findlimit(vela,num=52)
+fvmax,fvmin=vasp.findlimit(vela,num)
 
 vasp.writepos('POSCAR','pos_vmax',posa[fvmax],vela[fvmax])
-vasp.writepos('POSCAR','pos_vmin',posa[fvmax],vela[fvmax])
+vasp.writepos('POSCAR','pos_vmin',posa[fvmin],vela[fvmin])
+
+shutil.copy('pos_vmax','../pos_vmax')
+shutil.copy('pos_vmin','../pos_vmin')
