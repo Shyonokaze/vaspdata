64301fe422e0042e45389fa7d11b23c3988d0273
Shyonokaze <vinceyuhaopan@sina.com>
2017-09-28 23:31:28 +0800

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -7,13 +7,11 @@
 export OPEN_MPI=/public/software/mpi/openmpi/1.6.5/intel
 export PATH=${OPEN_MPI}/bin:$PATH
 export INCLUDE=${OPEN_MPI}/include:$INCLUDE
-export LD_LIBRARY_PATH=${OPEN_MPI}/lib:${OPEN_MPI}/share:/public/software/compiler/intel/composer
-_xe_2015.2.164/compiler/lib/intel64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=${OPEN_MPI}/lib:${OPEN_MPI}/share:/public/software/compiler/intel/composer_xe_2015.2.164/compiler/lib/intel64:$LD_LIBRARY_PATH
 
 cd $PBS_O_WORKDIR
 NP=`cat $PBS_NODEFILE | wc -l`
 
 num=88
 
-python /public/home/users/ruc001/work/7-PYH/7-BlackP-MD/1-MD/2aBest/main.py $NP $PBS_NODEFILE $nu
-m
+python /public/home/users/ruc001/work/7-PYH/7-BlackP-MD/1-MD/2aBest/main.py $NP $PBS_NODEFILE $num
